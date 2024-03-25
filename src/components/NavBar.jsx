e70aa5dd49f842fb1311708f66461254a18961d7
GonzaloG28 <gonzalogimenez280@gmail.com>
2024-03-25 14:46:03 -0300

    añadi el componente navBar y elimine las hojas de estilo del main y de app, y añadi un nuevo archivo css

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import "../style.css"
+
+function NavBar() {
+  return (
+    <div>
+        <nav className="navBar">
+            <div><img src="" alt="" /></div>
+            <ul>
+                <li><a href="">Inicio</a></li>
+                <li><a href="">Productos</a></li>
+                <li><a href="">Contacto</a></li>
+                <li><a href="">Sobre nosotros</a></li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default NavBar
